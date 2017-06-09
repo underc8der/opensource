3989c7f1dbba56de25e6eb76e6d489f2854ed2bc
David Gonzalez <davidn.g0015@gmail.com>
2017-06-08 20:37:44 -0500

    records sorting and refactoring.

--- src/opensource/ProjectsReader.java
+++ src/opensource/ProjectsReader.java
@@ -7,17 +7,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.TreeSet;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -28,83 +24,76 @@ public class ProjectsReader {
 	private static final String OUTPUT = "src/summary.txt";
 	private static final String IS_PROJECT = "([A-Z]+\\s*)+";
 	
-	private SortedMap<String, Set<String>> projects = new TreeMap<>();
+	private List<Map.Entry<String, Set<String>>> summary;
 	
-	public List<String> readProjects() throws IOException {
+	private List<String> readProjectsFile() throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(INPUT));
 		List<String> result = reader.lines().collect(Collectors.toList());
 		reader.close();
 		return result;
 	}
 	
-	private void summarizeProjects(List<String> data) {
-		List<String> keys = data.stream()
+	public void summarizeProjects() throws IOException {
+		List<String> data = readProjectsFile();
+		
+		List<String> projects = data.stream()
 				.filter(rec -> rec.matches(IS_PROJECT))
 				.collect(Collectors.toList());
 		
-		Map<String, Set<String>> projectsXUserid = new TreeMap<>();
-		IntStream.range(0, keys.size()).forEach(i -> {
-			projectsXUserid.put(keys.get(i), 
-				new HashSet<>(data.subList(data.indexOf(keys.get(i)) + 1, 
-					keys.size() > (i + 1) ? data.indexOf(keys.get((i + 1))) : data.size())));
+		Map<String, Set<String>> projectsXUID = new TreeMap<>();
+		IntStream.range(0, projects.size()).forEach(i -> {
+			projectsXUID.put(projects.get(i), 
+				new HashSet<>(data.subList(data.indexOf(projects.get(i)) + 1, 
+					projects.size() > (i + 1) ? data.indexOf(projects.get((i + 1))) : data.size())));
 		});
 		
-		Map<String, Long> usersidCounter = projectsXUserid.values().stream()
-				.flatMap(x -> x.stream())
+		Map<String, Long> uidGrouped = projectsXUID.values().stream()
+				.flatMap(uid -> uid.stream())
+				.filter(uid -> !uid.equals("0") && !uid.equals("1"))
 				.collect(Collectors.groupingBy(k -> k, Collectors.counting()));
 		
-		List<String> noDuplicated = usersidCounter.entrySet().stream()
-				.filter(map -> map.getValue() <= 1)
-				.map(map -> map.getKey())
+		List<String> uidNonDuplicates = uidGrouped.entrySet().stream()
+				.filter(uids -> uids.getValue() <= 1)
+				.map(uids -> uids.getKey())
 				.collect(Collectors.toList());
 		
-		projectsXUserid.values().stream().forEach(usersid -> {
-			usersid.retainAll(noDuplicated);
+		projectsXUID.values().stream().forEach(usersid -> {
+			usersid.retainAll(uidNonDuplicates);
 		});
 		
-//		Map<String, Long> summary = projectsXUserid.values().stream()
-//				.flatMap(x -> x.stream())
-//				.collect(Collectors.groupingBy(k -> k, Collectors.counting()));
-		
-		System.out.println("shit" + sortProjects(projectsXUserid));
+		summary = sortProjects(projectsXUID);
 	}
 	
 	private List<Map.Entry<String, Set<String>>> sortProjects(Map<String, Set<String>> summary) {
-		List<Map.Entry<String, Set<String>>> sorted =
+		List<Map.Entry<String, Set<String>>> sortedProjects =
                 new LinkedList<Map.Entry<String, Set<String>>>(summary.entrySet());
 		
-		Collections.sort(sorted, new Comparator<Map.Entry<String, Set<String>>>() {
+		Collections.sort(sortedProjects, new Comparator<Map.Entry<String, Set<String>>>() {
 			@Override
 			public int compare(Entry<String, Set<String>> o1, Entry<String, Set<String>> o2) {
 				return Long.valueOf(o2.getValue().size()).compareTo(Long.valueOf(o1.getValue().size()));
 			}
 		});
 		
-		return sorted
-				//.stream().collect( Collectors.toMap(Function.identity(), x -> x))
-				;
+		return sortedProjects;
 	}
 	
 	private void writeFile() throws IOException {
 		PrintWriter pw = new PrintWriter(new FileWriter(OUTPUT));
-		projects.forEach((k, v) -> {
-			pw.println(k + " " + v.size());
+		summary.stream().forEach(v -> {
+			pw.println(v.getKey() + " " + v.getValue().size());
 		});
     	pw.close();
 	}
 	
+	public List<Map.Entry<String, Set<String>>> getProjects() {
+		return summary;
+	}
+
 	public static void main(String[] args) throws IOException {
 		ProjectsReader reader = new ProjectsReader();
-		reader.summarizeProjects(reader.readProjects());
+		reader.summarizeProjects();
 		reader.writeFile();
-		System.out.println(reader.getProjects());
-	}
-
-	public SortedMap<String, Set<String>> getProjects() {
-		return projects;
-	}
-
-	public void setProjects(SortedMap<String, Set<String>> projects) {
-		this.projects = projects;
+		System.out.println("done...");
 	}
 }
